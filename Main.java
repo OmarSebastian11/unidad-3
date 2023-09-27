11406df8bf94b9849c0c4f3d335e9eae9ca1ae68
OmarSebastian11 <143815501+OmarSebastian11@users.noreply.github.com>
2023-09-26 23:22:24 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+     Scanner ent = new Scanner (System.in);
+	 
+	 
+	     System.out.println("introduce el numero");
+	     N = ent.nextInt ();
+	 
+	 int N;    
+	 if(N % 2 == 0 ){
+	 System.out.println("es par");
+	 }
+	 else{
+	     System.out.println("Es Impar");
+	 }
+	}
+}
